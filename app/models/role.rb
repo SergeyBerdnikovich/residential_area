0b249788929c85354682edec0919369ab63a181d
SergeyBerdnikovich <atom@tut.by>
2013-01-10 15:53:19 +0300

    update model role and user

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,7 @@ class Role < ActiveRecord::Base
   has_many :users
 
   attr_accessible :name
+
+  validates :name, :presence => true,
+                   :length => { :minimum => 2, :maximum => 20 }
 end
