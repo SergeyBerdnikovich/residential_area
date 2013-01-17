1a0b6ebca79d40d9498cffe150434aff0be5a248
SergeyBerdnikovich <atom@tut.by>
2013-01-17 16:20:19 +0300

    update admin-heads controller spec

--- spec/controllers/admin/heads_controller_spec.rb
+++ spec/controllers/admin/heads_controller_spec.rb
@@ -6,12 +6,29 @@ describe Admin::HeadsController do
   render_views
 
   let(:head) { create(:head) }
+  let(:location) { create(:location) }
+  let(:phone_number) { create(:phone_number) }
   before do
     login_as create(:user)
   end
   describe "GET 'edit'" do
-    it 'page should have content "Head was successfully updated"', :vcr do
+    it 'page(without phone_numbers, location) should have content "Head was successfully updated"', :vcr do
 
+      visit edit_admin_head_path(head)
+      fill_in('ФИО', :with => 'Никитин Валерий Павлович')
+      fill_in('Должность', :with => 'начальник склада №3')
+      fill_in('Number', :with => '+37529 339 76 39')
+      fill_in('Name', :with => 'Дом начальника склада №3')
+      fill_in('Address', :with => 'Калинина 17, Минск')
+      click_button('Update Head')
+
+      page.should have_content("Head was successfully updated")
+    end
+
+    it 'page(with phone_numbers, location) should have content "Head was successfully updated"', :vcr do
+
+      head.phone_numbers << phone_number
+      head.location = location
       visit edit_admin_head_path(head)
       fill_in('ФИО', :with => 'Никитин Валерий Павлович')
       fill_in('Должность', :with => 'начальник склада №3')
