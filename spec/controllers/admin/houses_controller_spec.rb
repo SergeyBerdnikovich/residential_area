c1c34d57925a90f86ac1a79fd64cc70f1c251994
SergeyBerdnikovich <atom@tut.by>
2013-01-17 16:03:37 +0300

    updated admin-houses controller spec

--- spec/controllers/admin/houses_controller_spec.rb
+++ spec/controllers/admin/houses_controller_spec.rb
@@ -6,12 +6,31 @@ describe Admin::HousesController do
   render_views
 
   let(:house) { create(:house) }
+  let(:apartment) { create(:apartment) }
+  let(:location) { create(:location) }
+  let(:gallery) { create(:gallery) }
   before do
     login_as create(:user)
   end
   describe "GET 'edit'" do
-    it 'page should have content "House was successfully updated"', :vcr do
+    it 'page(without location, gallery) should have content "House was successfully updated"', :vcr do
 
+      visit edit_admin_house_path(house)
+      fill_in('Название', :with => 'Дом XXL')
+      fill_in('Описание', :with => '25-этажный дом')
+      check('Построен')
+      attach_file('Image', "#{Rails.root}/app/assets/images/rails.png")
+      fill_in('Name', :with => 'Дом XXL')
+      fill_in('Address', :with => 'Колоса 27, Минск')
+      click_button('Update House')
+
+      page.should have_content("House was successfully updated")
+    end
+
+    it 'page(with location, gallery) should have content "House was successfully updated"', :vcr do
+
+      house.location = location
+      house.galleries << gallery
       visit edit_admin_house_path(house)
       fill_in('Название', :with => 'Дом XXL')
       fill_in('Описание', :with => '25-этажный дом')
@@ -42,8 +61,9 @@ describe Admin::HousesController do
   end
 
   describe "GET 'show'" do
-    it 'page should have content "House Details"' do
+    it 'page(with apartments) should have content "House Details"' do
 
+      house.apartments << apartment
       visit admin_house_path(house)
 
       page.should have_content("House Details")
@@ -53,6 +73,7 @@ describe Admin::HousesController do
   describe "GET 'index'" do
     it 'page should have content "Houses"' do
 
+      house
       visit admin_houses_path
 
       page.should have_content("Houses")
