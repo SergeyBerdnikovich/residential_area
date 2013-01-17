6231f3f43a373c9e47e1dd420d966aa2672e13ac
SergeyBerdnikovich <atom@tut.by>
2013-01-17 16:13:55 +0300

    update admin apartments controller spec

--- spec/controllers/admin/apartments_controller_spec.rb
+++ spec/controllers/admin/apartments_controller_spec.rb
@@ -7,11 +7,30 @@ describe Admin::ApartmentsController do
 
   let(:apartment) { create(:apartment) }
   let(:house) { create(:house) }
+  let(:gallery) { create(:gallery) }
   before do
     login_as create(:user)
   end
   describe "GET 'edit'" do
-    it 'page should have content "Apartment was successfully updated"' do
+    it 'page(with gallery) should have content "Apartment was successfully updated"' do
+
+      house
+      apartment.galleries << gallery
+      visit edit_admin_apartment_path(apartment)
+      select(house.title, :from => 'apartment_house_id')
+      find_field('Название')
+      find_field('Описание')
+      fill_in('Цена', :with => 32000)
+      fill_in('Площадь', :with => 144)
+      fill_in('Кол. комнат', :with => 3)
+      uncheck('Жилое помещение')
+      attach_file('Image', "#{Rails.root}/app/assets/images/rails.png")
+      click_button('Update Apartment')
+
+      page.should have_content("Apartment was successfully updated")
+    end
+
+    it 'page(without gallery) should have content "Apartment was successfully updated"' do
 
       house
       visit edit_admin_apartment_path(apartment)
